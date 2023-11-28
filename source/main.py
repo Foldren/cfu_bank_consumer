11df51db4c2c6c8b69072827d81bb2378f446cc3
administrator <cpacash.business@gmail.com>
2023-11-28 14:40:34 +0300

    fix taskiq

--- source/main.py
+++ source/main.py
@@ -1,7 +1,6 @@
 from asyncio import run
 from faststream import FastStream
 from faststream.rabbit import RabbitBroker
-from taskiq_faststream import BrokerWrapper
 from tortoise import run_async
 from config import RABBITMQ_URL
 from init_db import init_db
@@ -9,7 +8,6 @@ from routers import manage_banks
 
 broker = RabbitBroker(RABBITMQ_URL)
 app = FastStream(broker)
-taskiq_broker = BrokerWrapper(broker)
 
 broker.include_router(manage_banks.router)
 
